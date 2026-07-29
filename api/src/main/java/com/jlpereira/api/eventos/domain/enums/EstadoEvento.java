b4476151ab728d54839af312420e108ca546f43e
Jose Luis Pereira <jl24pereira@gmail.com>
2026-07-29 01:18:44 -0600

    chore: Se crean enums para eventos y funcion secuencial para codigo de evento

---
+++ api/src/main/java/com/jlpereira/api/eventos/domain/enums/EstadoEvento.java
@@ -0,0 +1,13 @@
+package com.jlpereira.api.eventos.domain.enums;
+
+/**
+ *
+ * @author Jose Luis Pereira
+ */
+public enum EstadoEvento {
+    NUEVO,
+    ASIGNADO,
+    EN_PROCESO,
+    RESUELTO,
+    CERRADO
+}
